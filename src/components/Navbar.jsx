411e4cc8c094e3795ddca6039c8f1ffbc29f95e7
shane <shane@groundedpackaging.co>
2022-10-14 15:36:34 +0700

    fix link bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <a href="https://www.gomugomu.art" className="flex items-center">
+          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Gomugomu logo" />
           <span className="self-center text-xl font-semibold whitespace-nowrap ">
             GomuGomu
           </span>
